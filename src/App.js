6fb4e7a14fddd3c82c0685b3e9c8dbd1d703a22f
Mohammed Amin <coldprohor12@gmail.com>
2024-10-17 17:23:37 -0700

    merged main with  submission branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // src/App.js
 import React from "react";
 import './App.css';
+import Map from "./components/Map/Map";
+import Submission from "./components/Submission/Submission";
 
 function App() {
   return (
@@ -8,6 +10,14 @@ function App() {
       <header className="App-header">
         <h2 className = "neon-rainbow">Wifi Monitor</h2>
       </header>
+      <div className="App-content">
+        <div className="submissionComponents">
+            <Submission />
+        </div>
+        <div className="Map-container">
+          <Map />
+        </div>
+      </div>
     </div>
   );
 }
